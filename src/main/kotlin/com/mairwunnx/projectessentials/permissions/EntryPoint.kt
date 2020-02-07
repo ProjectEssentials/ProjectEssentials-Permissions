3fffeaa4675292df7af4a456480c866720fb8f8f
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-02-07 22:19:35 +0300

    EntryPoint.kt version updated.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/permissions/EntryPoint.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/permissions/EntryPoint.kt
@@ -21,7 +21,7 @@ internal class EntryPoint : EssBase() {
 
     init {
         modInstance = this
-        modVersion = "1.14.4-1.1.0.0"
+        modVersion = "1.15.2-1.0.0"
         logBaseInfo()
         validateForgeVersion()
         MinecraftForge.EVENT_BUS.register(this)
